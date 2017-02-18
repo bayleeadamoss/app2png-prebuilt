d2db71a6ee17b3e38315f5186044aae846af9153
Blaine Schmeisser <blainesch@gmail.com>
2017-02-18 12:27:20 -0800

    Add better error handling.

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ module.exports = {
     return new Promise((resolve, reject) => {
       const progPath = path.join(process.cwd(), 'build', 'app2png-rs')
       const app2png = spawn(progPath, [appFileInput, pngFileOutput])
+      const err = []
+      app2png.stderr.on('data', (data) => err.push(data))
       app2png.on('close', (code) => {
-        code === 0 ? resolve() : reject()
+        code === 0 ? resolve() : reject(err.join(''))
       })
     })
   }
--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const app2png = require('./')
 app2png.convert('/Applications/Messages.app', './Messages.png').then(() => {
   console.log('done')
   process.exit(0)
-}).catch(() => {
-  console.log('failed')
+}).catch((error) => {
+  console.log('failed', error)
   process.exit(1)
 })
